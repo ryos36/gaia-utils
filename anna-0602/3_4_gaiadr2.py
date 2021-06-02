1ad403501957afe8c04498fc80b4c0800593dc7b
Ryos Suzuki <ryos@sinby.com>
2021-06-02 14:17:36 +0900

    ここにあるデータで動くようにした。

--- anna-0602/3_4_gaiadr2.py
+++ anna-0602/3_4_gaiadr2.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import numpy as np 
-df1=pd.read_csv('catalog-xu2021.csv', sep=',')
-df2=pd.read_csv('gaia_dr2_white_dwarf_candidates_v2.csv', sep=',')
+
+target_file = '../anna-0530/data/target.csv'
+key1='source_id'
+selected_file = '../anna-0530/data/selected2.csv'
+key2='gaiaID'
+key2='source_id'
+result_file = 'result.csv'
+result_file = 'result2.csv'
+
+df1=pd.read_csv(target_file, sep=',')
+df2=pd.read_csv(selected_file, sep=',')
+
 #print(df1['gaiaID'])
 #print(df2['source_id'])
+
 df = pd.DataFrame([])  
-for gaiaID in df1['gaiaID']:
-    match = df2[df2['source_id'] == int(gaiaID) ].index
+for gaiaID in df1[key2]:
+    match = df2[df2[key1] == int(gaiaID) ].index
     #if len(match) != 1:
         #print(gaiaID)
     data=df2.loc[match,:]
     df=df.append(data)
-   
-​
-​
-df.to_csv('crsmtach_Xu_Fusillo.csv', index=False)
+
+df.to_csv(result_file, index=False)
