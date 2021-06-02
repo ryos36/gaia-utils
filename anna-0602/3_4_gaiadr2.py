8b489b09615ea402c3276fe1bff21c259376044c
Ryos Suzuki <ryos@sinby.com>
2021-06-02 18:02:00 +0900

    リストの append と concat を使う方が3倍くらい速かった。

--- anna-0602/3_4_gaiadr2.py
+++ anna-0602/3_4_gaiadr2.py
@@ -3,24 +3,26 @@ import numpy as np
 
 target_file = '../anna-0530/data/target.csv'
 key1='source_id'
-selected_file = '../anna-0530/data/selected2.csv'
-key2='gaiaID'
+#selected_file = '../anna-0530/data/selected2.csv'
+selected_file = '../anna-0530/data/target.csv'
+#key2='gaiaID'
 key2='source_id'
 result_file = 'result.csv'
-result_file = 'result2.csv'
 
 df1=pd.read_csv(target_file, sep=',')
+#df2=pd.read_csv(selected_file, sep=',').sort_values(key2)
 df2=pd.read_csv(selected_file, sep=',')
 
 #print(df1['gaiaID'])
 #print(df2['source_id'])
 
-df = pd.DataFrame([])  
+lst=[]
 for gaiaID in df1[key2]:
     match = df2[df2[key1] == int(gaiaID) ].index
     #if len(match) != 1:
         #print(gaiaID)
     data=df2.loc[match,:]
-    df=df.append(data)
+    lst.append(data)
+df = pd.concat(lst)
 
 df.to_csv(result_file, index=False)
